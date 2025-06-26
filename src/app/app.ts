ed6ffe91e126ead44c3ae1dd48f50c6bf71a0fec
CarlosServin18 <137239200+CarlosServin18@users.noreply.github.com>
2025-06-26 00:24:15 -0600

    arreglo de  errores en la página de inicio agragacion de memu

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,CommonModule,RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
